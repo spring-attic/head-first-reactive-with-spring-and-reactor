253925f728f0dcc6e40f5d2371b0d82326a12971
Brian Clozel <bclozel@pivotal.io>
2017-05-09 23:29:52 +0200

    Integration tests with WebTestClient

--- stock-quotes/src/test/java/io/spring/workshop/stockquotes/StockQuotesApplicationTests.java
+++ stock-quotes/src/test/java/io/spring/workshop/stockquotes/StockQuotesApplicationTests.java
@@ -2,15 +2,40 @@ package io.spring.workshop.stockquotes;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//  We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StockQuotesApplicationTests {
 
+	// Spring Boot will create a `WebTestClient` for you,
+	// already configure and ready to issue requests against "localhost:RANDOM_PORT"
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	public void contextLoads() {
+	public void fetchQuotes() {
+		webTestClient
+				// We then create a GET request to test an endpoint
+				.get().uri("/quotes?size=20")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				// and use the dedicated DSL to test assertions against the response
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBodyList(Quote.class)
+				.hasSize(20)
+				// Here we check that all Quotes have a positive price value
+				.consumeWith(allQuotes ->
+						assertThat(allQuotes).allSatisfy(quote -> assertThat(quote.getPrice()).isPositive()));
 	}
 
 }
